56bc606142f7d545f61e2d3de05c6b0adeed4105
Atif Patel <atif.patel@matillion.com>
2024-03-01 14:03:00 +0000

    add references

--- learning-tool/src/components/arrayChoiceButtons/selectArray/selectArray.jsx
+++ learning-tool/src/components/arrayChoiceButtons/selectArray/selectArray.jsx
@@ -22,7 +22,7 @@ function SelectArray({setArray, setArraySize, setInitialArray}) {
             </button>
             {showDropdown && (
                 <div className="dropDownMenu">
-                    <select  value={stringArray} onChange={handleArrayChange} size="6">
+                    <select  value={stringArray} onChange={handleArrayChange} size="3">
                         <option value=" " >empty</option>
                         <option value="305,300,270,202,200,170,115,103,100,95">low to high</option>
                         <option value="270,109,30,170,46,87,93,95">small</option>
@@ -32,6 +32,7 @@ function SelectArray({setArray, setArraySize, setInitialArray}) {
                         <option value="225,220,215,210,205,210,215,220,225">peak in the middle with troughs</option>
                         <option value="210,215,220,225,220,215,210">symmetric peak</option>
                         <option value="205,215,225,235,245,235,225,215,205">zigzag pattern</option>
+                        <option value="27,67,81,107,125,287,164,139,90">partially sorted</option>
                     </select>
                 </div>
             )}
